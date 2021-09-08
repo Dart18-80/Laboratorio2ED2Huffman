8bc10f95fdd4d50a9456ccee1487abf5e080d299
randres <randr@DESKTOP-DVJNG83>
2021-09-08 00:35:21 -0600

    Llamado program

---
+++ ConsolaHuffman/Comparable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolaHuffman
+{
+    class Comparable : IComparable
+    {
+        public int CompareToNumero(int num1, int num2)
+        {
+            return num1.CompareTo(num2);
+        }
+        public int CompareTo(object obj)
+        {
+            if (Convert.ToInt16(this.CompareTo(obj)) > 0)
+                return 1;
+            else if (Convert.ToInt16(this.CompareTo(obj)) < 0)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ ConsolaHuffman/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ArbolesDeHuffman;
+namespace ConsolaHuffman
+{
+    class Program
+    {
+        delegate int DelegadosN(int Numero1, int Numero2);
+
+        static void Main(string[] args)
+        {
+            ColaDePrioridad<int> ArbolHuff = new ColaDePrioridad<int>();
+            NodoCP<int> nodo = new NodoCP<int>(); 
+            Comparable CallNum = new Comparable();
+            DelegadosN InvocarNumero = new DelegadosN(CallNum.CompareToNumero);//llamado del delegado
+
+            Console.WriteLine("Hello World!");
+            bool verificacion = true;
+            while (verificacion)
+            {
+                Console.WriteLine("Ingrese un numero");
+                int num = Convert.ToInt32(Console.ReadLine());
+                
+
+                Console.WriteLine("salida??");
+                int salida = Convert.ToInt32(Console.ReadLine());
+                if (salida==2)
+                {
+                    verificacion = false;
+                }
+            }
+        }
+    }
+}
