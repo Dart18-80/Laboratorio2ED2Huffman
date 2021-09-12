0778b56ece764894728529e3d5d6d9f77d91127e
randres <randr@DESKTOP-DVJNG83>
2021-09-12 03:03:20 -0600

    Lectura de txt

---
+++ Laboratorio2ED2/Controllers/CodificacionController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Laboratorio2ED2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CodificacionController : ControllerBase
+    {
+        public readonly IHostingEnvironment fistenviroment;
+        public CodificacionController(IHostingEnvironment enviroment)
+        {
+            this.fistenviroment = enviroment;
+        }
+        public class FileUlploadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+
+        [HttpPost]
+
+        public IActionResult Post([FromForm] IFormFile files) //para poder llamarlo se necesita poner el noombre files en la key
+        {
+            if (files!=null)
+            {
+                string uploadsFolder = Path.Combine(fistenviroment.ContentRootPath, "Upload");
+                string filepath = Path.Combine(uploadsFolder, files.FileName);
+                if (!System.IO.File.Exists(filepath))
+                {
+                    using (var INeadLearn = new FileStream(filepath, FileMode.CreateNew))
+                    {
+                        files.CopyTo(INeadLearn);
+                    }
+                }
+                string ccc = System.IO.File.ReadAllText(filepath);// es el texto del archivo de texto
+
+            }
+            return Ok();
+        }
+    }
+}
