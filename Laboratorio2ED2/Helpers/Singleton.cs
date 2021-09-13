8bc9028a6a8d3b72ce379d51819b5029451b121b
Diego Ruiz <diegoruiztrabanino13@gmail.com>
2021-09-12 19:05:00 -0600

    Singleton

---
+++ Laboratorio2ED2/Helpers/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio2ED2.Helpers
+{
+    public class Singleton
+    {
+        private static Singleton _intance;
+        public static Singleton Intance
+        {
+            get
+            {
+                if (_intance == null) _intance = new Singleton();
+                return _intance;
+            }
+        }
+
+        //Tenes que insertar aqui lo que queres guardar 
+
+
+
+    }
+}
