0f62c2c1557c8a5d0292abe77d47d1136e73147b
Diego Ruiz <diegoruiztrabanino13@gmail.com>
2021-09-13 22:40:39 -0600

    Descompresion

--- Laboratorio2ED2/Models/Letras.cs
+++ Laboratorio2ED2/Models/Letras.cs
@@ -34,6 +34,18 @@ namespace Laboratorio2ED2.Models
                 return 1;
             }
         }
+
+        public int Descompresion (Letras x, char y)
+        {
+            if (x.Binario == Convert.ToString(y))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
         public int CompareToSalida(Letras x)
         {
             if (x.index > 0.8 && x.index < 1.2)
